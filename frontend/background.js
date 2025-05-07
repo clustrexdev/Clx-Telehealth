4ffe765d0348d21ac22fb4fe537d3165e5ef9dd4
TKarthikeyan-Clustrex <tkarthik@clustrex.com>
2025-05-07 19:50:47 +0530

    Major Changes in Extension's UI and action flows.

--- frontend/background.js
+++ frontend/background.js
@@ -1,22 +1,59 @@
-
-// Allows users to open the side panel by clicking on the action toolbar icon
-chrome.sidePanel
-    .setPanelBehavior({ openPanelOnActionClick: true })
-    .catch((error) => console.error(error));
+const EMRBaseURL = "https://preview.athenahealth.com/"
 
 
+// On clicking the toolbar icon, it checks whether it is the correct page and 
+// then Sidepanel will be opened and extract data function will be injected.
+chrome.action.onClicked.addListener((tab) =>{
+    if (validateURL(tab.url)){
+        chrome.sidePanel.open({ tabId : tab.id }, () => {
+            console.log("Side Panel Opened");
+        });
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id, allFrames: true },
+            func: extractData
+        });
+    }
+});
+
+
+// Validate the URL
+function validateURL(url) {
+    return url.startsWith(EMRBaseURL);
+}
+
+
+// Shows the sidebar for the valid URL and hides for the other URLs.
+function handleTabChange(tabId) {
+    chrome.tabs.get(tabId, async (tab) => {
+        await chrome.sidePanel.setOptions({
+            tabId,
+            path: 'sidepanel.html',
+            enabled: validateURL(tab.url)
+        });
+    });
+}
+
+
+// When user switches tabs
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    handleTabChange(activeInfo.tabId);
+});
+
+
+// When the URL of a tab changes
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url) {
+        handleTabChange(tabId);
+    }
+});
+
+
+// Listens to the scrapeData message request raised when the patient data record button is clicked.
 chrome.runtime.onMessage.addListener((message) => {
     if (message.action === "scrapeData") {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             if (!tabs.length) return;
             const tab = tabs[0];
-
-            // To exclude adding scripts to chrome default pages
-            if (tab.url.startsWith('chrome://') || tab.url.startsWith('chrome-extension://')) {
-                console.warn("Restricted page. Skipping script injection.");
-                return;
-            }
-
             chrome.scripting.executeScript({
                 target: { tabId: tab.id, allFrames: true },
                 func: extractData
@@ -26,19 +63,27 @@ chrome.runtime.onMessage.addListener((message) => {
 });
 
 
+// Extracts the necessary fields from the injected page (on the left side) and 
+// returns the scraped values to the sidebar (right side extension).
 function extractData() {
-    const patientId = document.getElementsByName("PATIENTID")?.[0]?.value || 'Not found';
-    const firstName = document.querySelector('#FIRSTNAME')?.value || 'Not found';
-    const lastName = document.querySelector('#LASTNAME')?.value || 'Not found';
-    const dob = document.querySelector('#dob')?.value || 'Not found';
+    // This frame only contains the necessary data.
+    if (window.frameElement?.id === 'frMain') {
+        const patientId = document.getElementsByName("PATIENTID")?.[0]?.value || 'Not found';
+        const firstName = document.querySelector('#FIRSTNAME')?.value || 'Not found';
+        const lastName = document.querySelector('#LASTNAME')?.value || 'Not found';
+        const dob = document.querySelector('#dob')?.value || 'Not found';
 
-    chrome.runtime.sendMessage({
-        type: 'return_data',
-        data: {
-            patientId: patientId,
-            firstName: firstName,
-            lastName: lastName,
-            dob: dob
-        }
-    });
+        // The sidepanel.js takes some latency to load, so we return the data after 500ms.
+        setTimeout(() => {
+            chrome.runtime.sendMessage({
+                type: 'return_data',
+                data: {
+                    patientId: patientId,
+                    firstName: firstName,
+                    lastName: lastName,
+                    dob: dob
+                }
+            });
+        }, 500);
+    }
 }
